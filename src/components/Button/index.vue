e8366475c94145f1c62cdbf36966267bfb5dcee2
Ali Afsahnoudeh <ali.afsah1988@gmail.com>
2020-09-12 09:26:36 +0430

    init

---
+++ src/components/Button/index.vue
@@ -0,0 +1,29 @@
+<template>
+  <button class="a-button">
+    {{ text }}
+  </button>
+</template>
+
+<script>
+export default {
+  name: 'Button',
+  props: {
+    text: {
+      type: String,
+      default: ''
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.a-button {
+  font-weight: 500;
+  outline: 0;
+  border: 0;
+  cursor: pointer;
+  text-decoration: none;
+  display: inline-block;
+  text-align: center;
+}
+</style>
